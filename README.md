348ec4a9d6a3961484acb211d9d71539cfc2dd87
RSGuy <63218795+ReSteGuy@users.noreply.github.com>
2023-04-10 21:34:34 -0400

    switchStateStuff

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Go down to the Psych Engine Installion for instructions!
 * RSGuy - Pretty much everything new! 
 * Shadow Mario & The whole team - Psych Engine Base
 
+## List
+- [X] Main Menu Option Disabling
+- [ ] Letters Ratings
+- [ ] Taunt
+- [ ] switchState()
+
 # Friday Night Funkin' - Psych Engine
 Engine originally used on [Mind Games Mod](https://gamebanana.com/mods/301107), intended to be a fix for the vanilla version's many issues while keeping the casual play aspect of it. Also aiming to be an easier alternative to newbie coders.
 
--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -37,6 +37,7 @@ typedef MainMenuData =
 class MainMenuState extends MusicBeatState
 {
 	public static var commonEngineVersion:String = '0.1.0'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = '0.6.3';
 	public static var curSelected:Int = 0;
 	public static var mainmenuJSON:MainMenuData;
 	
@@ -168,8 +169,10 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Common Engine v" + commonEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Common Engine v" + commonEngineVersion, 12);
 		versionShit.scrollFactor.set();
+		var versionPsychShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
+		versionShitPsych.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
